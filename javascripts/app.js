ba2a861f7a0931ebbeffa6fb2d9ba5cbd1b85f94
Yago Vega <yagofvv@gmail.com>
2018-03-03 14:28:44 +0100

    input validate in commands function, and moving backwards added

--- javascripts/app.js
+++ javascripts/app.js
@@ -86,6 +86,50 @@ function moveForward(rover) {
     rover.travelLog.push(coords.concat(y));
   }
 }
+function moveBackwards(rover){
+  var offGrid = false;
+  switch (rover.direction) {
+    case "N":
+      if (rover.y-1 < 0) {
+        offGrid = true;
+      } else {
+        rover.y--;
+      }
+      break;
+    case "E":
+      if (rover.x+-1 < 0) {
+        offGrid = true;
+      } else {
+        rover.x--;
+      }
+      break;
+    case "S":
+      if (rover.y+1 > 9) {
+        offGrid = true;
+      } else {
+        rover.y++;
+      }
+      break;
+    case "W":
+      if (rover.x+1 > 9) {
+        offGrid = true;
+      } else {
+        rover.x++;
+      }
+      break;
+  }
+  if (offGrid) {
+    console.log("The rover cannot go backwards! Limits of the grid reached!");
+  } else {
+    console.log("Moved backwards!");
+    var x = rover.x.toString();
+    var space = " ";
+    var y = rover.y.toString();
+    var coords = x.concat(space);
+    rover.travelLog.push(coords.concat(y));
+  }
+
+}
 
 function commands(commands) {
   for (var i = 0; i < commands.length; i++) {
@@ -99,6 +143,12 @@ function commands(commands) {
         break;
       case "l":
         turnLeft(rover);
+        break;
+      case "b":
+        moveBackwards(rover);
+        break;
+      default: 
+        console.log("Unknown command!");
         break;
     }
   }
